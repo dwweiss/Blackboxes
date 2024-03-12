a8de1699b6a06fde304f5a7bedb00addd813706f
Dietmar W. Weiss <dwweiss@users.noreply.github.com>
2024-03-12 13:00:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
 setup(
+    author='Dietmar Wilhelm Weiss',
+    author_email='dietmar.wilhelm.weiss@gmail.com',
+    description='Unified interface to popular neural network libraries',
+    install_requires = [numpy, matplotlib, scipy, keras, torch, neurolab],
+    keywords='neural network, black box, empirical model',
+    license='GNU lesser license',
+    long_description="\n\n".join([README, CHANGES]),
     name='blackboxes',
-    version='03.24',
     packages=['blackboxes'],
-    url='',
-    license='',
-    author='dww',
-    author_email='',
-    description='Unified access to neural network libraries'
+    url='https://github.com/blackboxes/blackboxes',
+    version = '03.24',
 )
+
